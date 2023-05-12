8181d333f8942140340738570fa59b0bca26c883
Sadık Ahmet Özdemir <you@example.com>
2023-05-12 18:02:49 +0300

    todo aktarıldı

---
+++ todo/assets/js/todo.js
@@ -0,0 +1,5 @@
+// let Ad = document.querySelector(`[name="KullaniciAdi"]`);
+
+document.querySelector('#todoButton').addEventListener('click', function() {
+    alert(document.querySelector('[name="Todo"]').value);
+})
