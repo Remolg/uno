82debb8e3631c364b3db4306061049af166d801c
Remolg <sadikahmet25@gmail.com>
2023-05-14 01:26:19 +0300

    sadece yapay zeka kismi kaldi

--- uno/assets/js/nuno.js
+++ uno/assets/js/nuno.js
@@ -1,88 +1,127 @@
+// Kartlar - biz - karsi oyuncu - cekilecek kartlar ama icini simdilik bos birakiyoruz 
 let cards = [];
 let player = [];
 let cpu = [];
 let deck = [];
 let table;
 
-// Kartları olusturma ve cards dizisine pushlama
+// For dongusu yapip kartlari olusturuyoruz olusturdugumuz kartlari card dizisine pushluyoruz 
 
-for(let i = 0; i <= 9; i++) {
+for (let i = 0; i <= 9; i++) {
     cards.push({ color: 'green', number: i.toString() });
     cards.push({ color: 'yellow', number: i.toString() });
     cards.push({ color: 'red', number: i.toString() });
     cards.push({ color: 'blue', number: i.toString() });
 }
 
-// Kartları karıştırma
+// olusturdugumuz kartlari karistiriyoruz 
 
-cards.sort(function() {
+cards.sort(function () {
     return Math.random() - 0.5
 });
 
-// Kartları dağıtma
+// karistirdigimiz kartlari for dongusu ile dagitiyoruz ve cards dizisine pushluyoruz shift ile yapmamizin nedeni dizinin 0. elementini almasidir 
 
-for(let i = 1; i <= 7; i++) {
+for (let i = 1; i <= 7; i++) {
     player.push(cards.shift());
     cpu.push(cards.shift());
 }
 
-// Masa kartını verme
+// bu kisimda table a cards dizisinin 0. elementi ataniyor 
 
 table = cards.shift();
 
-// Kalan kartlar
+// bu kisimda ise kalan kartlarin tamami deck elementine ataniyor 
 
 deck = cards;
 
-// Sıra
+// sira belirleniyor 
 
 let turn = 1;
 
-// Kartları html ye yazdırma
+// olusturdugumuz kartlari divlere atama ve bunu createCardHtml fonkisyonuna atama <-- data-color ve data-number de olusturulur icerisine verileri atilr -->
 
-function createCardHtml(color, number) {
-    return `<div class="card ${color}" data-color="${color}" data-number="${number}">${number}</div>`;
+function createCardHtml(color, number, back = false) {
+    return `<div class="card ${color} ${back ? 'back' : ''}" data-color="${color}" data-number="${number}">${number}</div>`;
 }
 
-// Html de ki classları secme ve onları atama 
+// html icersinden secilen calsslari bos nesnelere atama
 
 const cpuContainer = document.querySelector('.cpu');
 const tableContainer = document.querySelector('.table');
 const playerContainer = document.querySelector('.player');
 const deckContainer = document.querySelector('.deck');
 
-// Kartları dizi içerisinden HTML e yazdırma
+// for islemi ile card diye bos bir eleman acilir ve player icerisinde ne kadar eleman var ise o kadar donmesi saglanir her donuste playerin icerisindeki bilgileri tek tek 
+// playercontainer.innerhtml ile oyuncuya verilir
 
-for(let card of player) {
+
+for (let card of player) {
     playerContainer.innerHTML += createCardHtml(card.color, card.number);
 }
 
-for(let card of cpu) {
-    cpuContainer.innerHTML += createCardHtml(card.color, card.number);
+// cpuContainer.innerhtml ile bilgisayara verilir
+
+for (let card of cpu) {
+    cpuContainer.innerHTML += createCardHtml(card.color, card.number, true);
 }
 
-for(let card of deck) {
-    deckContainer.innerHTML += createCardHtml(card.color, card.number);
+// deckContainer.innerhtml ile cekilecek kartlara eklenir ve true metodu ile arkasi cevrilir
+
+for (let card of deck) {
+    deckContainer.innerHTML += createCardHtml(card.color, card.number, true);
 }
 
-// Masada ki kartı yazdırma 
+// tableContainer.innerhtml ile tahtaya baslangic karti koyulur
 
 tableContainer.innerHTML += createCardHtml(table.color, table.number);
 
-
-// Bütün card classlarını cardElementse aktar
+// butun card classlari cardelemetse atanir 
 
 let cardElements = document.querySelectorAll('.card');
 
-for(let cardElement of cardElements) {
+// for dongusu ile hepsini tiklanilabilir yapilir 
+
+for (let cardElement of cardElements) {
     cardElement.addEventListener('click', playCard);
 }
 
-// Kart eğer oynanabilir ise isCardPlayable içine true değil ise false yaz ve geç
+// kartin oynanabilir olup olmamasini kontrol eder tahtadaki kart ile oyuncudaki kartlari kiyaslar
 
 function isCardPlayable(cardColor, cardNumber) {
-    if(table.color === cardColor || table.number === cardNumber) {
+    if (table.color === cardColor || table.number === cardNumber) {
         return true;
+    }
+
+    return false;
+}
+
+// changeTurn ile siranin kimde oldugunu belirler ve sirayi degistirir 1 veya 2 degil ise is deck false olur 
+
+function changeTurn() {
+    if (turn === 1) {
+        turn = 2;
+    } else {
+        turn = 1;
+    }
+    isDeckUsed = false;
+}
+
+// canCurrentUserPlay fonksiyonu siradaki kisiyi belirler ve o kisinin bilgilerini currentPlayerCards a atar atanan bilgiler tek tek doner eger kart oynanabilir ise 
+// fonksiyonunda kartlarin rengi ve sayisi kontrol ettirilir oynanabilir ise true doner oynanamaz ise false doner 
+
+function canCurrentUserPlay() {
+    let currentPlayerCards;
+    if (turn === 1) {
+        currentPlayerCards = document.querySelectorAll('.player .card');
+    } else {
+        currentPlayerCards = document.querySelectorAll('.cpu .card');
+    }
+
+    for (let card of currentPlayerCards) {
+        if (isCardPlayable(card.dataset.color, card.dataset.number)) {
+            return true;
+        }
     }
 
     return false;
@@ -90,27 +129,32 @@ function isCardPlayable(cardColor, cardNumber) {
 
 let isDeckUsed = false;
 
-// Oyun başlıyor
+// oyun seansi baslar ilk olarak 
 
 function playCard() {
-
-    // Table ı kontrol ediyor
-
-    if(this.parentNode.classList.contains(`table`)){
+    // table classina tiklanirsa olacaklar  
+    if (this.parentNode.classList.contains('table')) {
         return;
     }
 
-    // Deck i kontrol ediyor 
+    // deck classina tiklanirsa olacaklar 
 
-    if(this.parentNode.classList.contains(`deck`)) {
+    if (this.parentNode.classList.contains('deck')) {
 
-        if(isDeckUsed === true){
+        // isdeckused true ise kart cekemessin 
+
+        if (isDeckUsed === true) {
             return;
         }
 
 
+        //  mevcut oyuncuda classinda back var ise siler ki kartlarimiz gozuksun 
 
-        if(turn === 1) {
+
+        // mecvut oyuncuya cekilen karti ekletir
+
+        if (turn === 1) {
+            this.classList.remove('back');
             playerContainer.appendChild(this);
         } else {
             cpuContainer.appendChild(this);
@@ -118,52 +162,75 @@ function playCard() {
 
         isDeckUsed = true;
 
-        return;
-    }
+        // mevcut oyuncu oynayabilecegi bir sey yok ise sirayi degistirir
 
-    if(!isCardPlayable(this.dataset.color, this.dataset.number)) {
-        return;
-    };
-
-    if(turn == 1 && this.parentNode.classList.contains(`cpu`)) {
-        return;
-    }
-
-    if(turn == 2 && this.parentNode.classList.contains(`player`)) {
-        return;
-    }
-
-
-    if(turn=== 1){
-        turn =2;
-    }else {
-        turn =1;
-    };
-
-    function canCurrentUserPlay() {
-        let currentPlayerCards;
-
-        if(turn === 1) {
-            currentPlayerCards = document.querySelectorAll(`.player .card`);
-        }else {
-            currentPlayerCards = document.querySelectorAll(`.cpu .card`);
+        if (!canCurrentUserPlay()) {
+            changeTurn();
         }
 
-        for(let card of currentPlayerCards) {
-            if(isCardPlayable(card.dataset.color, card.dataset.number)) {
-                return true;
-            }
-        }
-
+        return;
     }
 
-    isDeckUsed = false;
+    // tikladigin kart oynanabir degil ise donguyu bitir
 
+    if (!isCardPlayable(this.dataset.color, this.dataset.number)) {
+        return;
+    }
 
-    /////////////////////// EMMET ///////////////////////////
+    // sira birinci oyuncuda ise ve tikladigin kart karsidaki oyuncu ise donguyu bitir
+
+    if (turn === 1 && this.parentNode.classList.contains('cpu')) {
+        return;
+    }
+
+    // sira ikinci oyuncuda ise ve tikladigin kart karsidaki oyuncu ise donguyu bitir
+
+    if (turn === 2 && this.parentNode.classList.contains('player')) {
+        return;
+    }
+
+    // tikladigin kartin ust classini playerCardContainer a ata
+
+    let playerCardContainer = this.parentNode;
+
+    // tikladigin karti tableContainer altina ekle
 
     tableContainer.appendChild(this);
+
+    //karsi taraftaki kartlarin masaya atilinca gorunur olmasi
+
+    let gorunurYap = tableContainer.querySelectorAll('.card');
+
+    for (var i = 0; i < gorunurYap.length; i++) {
+        gorunurYap[i].classList.remove('back');
+    }
+
+
+    // table.coloru tikladigin kartin colorune esitle
+
     table.color = this.dataset.color;
+
+    // table.numberi tikladigin kartin numberina esitle
+
     table.number = this.dataset.number;
+
+    // tikladigin classin at classi 0 ise kazanan oyuncuyu soyle ve 
+
+    if (playerCardContainer.children.length === 0) {
+        alert('Kazanan Oyuncu: ' + turn);
+
+        // tiklanabilir butun kartlarin tiklama eventini kapatir
+
+        cardElements.forEach(card => card.removeEventListener('click', playCard));
+        return; // cok gerekli degil kaldirsak da durum degismez cunku zaten oyun bitti.
+    }
+
+    // sirayi karsiya ver 
+
+    changeTurn();
 }
 
+
+// cpu'nun elindeki karti oynatmak icin
+// [2] yazan yer dinamik olacak
+// document.querySelectorAll('.cpu .card')[2].click();
