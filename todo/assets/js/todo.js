eaa66c65240cc48d04e6fe23140da22f79fcd1f7
Remolg <sadikahmet25@gmail.com>
2023-05-24 01:36:23 +0300

    yazi ekleme yapildi

--- todo/assets/js/todo.js
+++ todo/assets/js/todo.js
@@ -1,120 +1,36 @@
-const todoListİnputElement = document.querySelector(".todoList");
-const todoListFormElement = document.querySelector(".TodoForm");
+const todoListElement = document.querySelector(".todoList");
+const todoFormElement = document.querySelector(".TodoForm");
+const todoInputElement = document.querySelector('[name="todo"]');
 
 
-function handleKeyDown(event) {
-    if (event.keyCode === 13) {
-        var inputElement = document.getElementById("inputText");
-        var inputText = inputElement.value;
-        console.log(inputText);
-        var listItem = document.createElement("li");
-        listItem.textContent = inputText;
-        todoListİnputElement.appendChild(listItem);
-        inputElement.value = "";
+todoInputElement.addEventListener("keydown", function (event) {
+
+  if (event.key === "Enter") {
+    event.preventDefault();
+    var value = todoInputElement.value.trim();
+
+    if (value !== "") {
+      var listItem = document.createElement("li");
+      var listInput = document.createElement("input");
+      listInput.type = "checkbox";
+      var listText = document.createElement("p");
+      listText.textContent = value;
+      var listRemoveButton = document.createElement("button");
+      var listStar = document.createElement("span");
+      listStar.innerHTML = "★";
+      listRemoveButton.innerHTML = "Sil";
+
+      listRemoveButton.addEventListener("click", function () {
+        listItem.remove();
+      })
+
+      listItem.appendChild(listInput);
+      listItem.appendChild(listText);
+      listItem.appendChild(listRemoveButton);
+      listItem.appendChild(listStar);
+      todoListElement.appendChild(listItem);
+      todoInputElement.value = " ";
     }
-    
   }
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const todoListElement = document.querySelector('.todoList');
-// const todoFormElement = document.querySelector('#TodoForm');
-// const todoInputElement = document.querySelector('[name="Todo"]');
-
-
-// const createTodoListHtml = todo => {
-//     // her todoitem yazıldıgında li elementi olusturulacak
-//     let todoItem = document.createElement('li');
-//     // her todoRemoveBtn yazıldıgında button elementi olusturulacak
-//     let todoRemoveBtn = document.createElement('button');
-//     // button içerisime x yazdırılıyor
-//     todoRemoveBtn.innerText = 'X';
-//     // her label yazıldığında label elementi oluşturulacak 
-//     let label = document.createElement('label');
-//     // label elementını todo ya ekliyor
-//     label.innerText = todo;
-//     // her changeInput yazıldığında input elementi oluşturulacak 
-    
-//     let changeInput = document.createElement('input');
-//     // changeInput un tipi text olarak belirleniyor
-//     changeInput.type = 'text';
-//     // changeInput elementını todo ya ekliyor
-//     changeInput.value = todo;
-    
-//     // changeInput elementi tetiklendiğinde olanlar 
-    
-//     changeInput.addEventListener('keydown', function(e) {
-//         // eğer tetikleyen elemen enter ise olacaklar
-//         if(e.key === 'Enter') {
-//             // tıklanınlan elementin ................
-//             this.previousSibling.innerText = this.value;
-//             // tıklanınlan elementin bir üstündeki elementin onEdit classını sil
-//             this.parentElement.classList.remove('onEdit');
-//         }
-//     });
-    
-//     // label elementine çift tıklandığıda olacaklar
-    
-//     label.addEventListener('dblclick', function() {
-//         // tıklanınlan elementin bir üstündeki elemente onEdit classını ekle
-//         this.parentElement.classList.add('onEdit');
-//         // tıklanılan elemente odaklanılmasını sağlar
-//         this.nextSibling.focus();
-//         // tıklanılan elementi seçmemizi sağlar
-//         this.nextSibling.select();
-//         // this.previousSibling.checked = this.previousSibling.checked ? false : true;
-//     });
-    
-//     // buton elemnetine tıklandıgında olacaklar
-    
-//     todoRemoveBtn.addEventListener('click', function() {
-//         // this.parentElement.remove();
-//         // todo item içerisindeki bütün elemenleri siler
-//         todoItem.remove();
-//     });
-    
-//     todoItem.innerHTML = `<input type="checkbox">`;
-//     todoItem.appendChild(label);
-//     todoItem.appendChild(changeInput);
-//     todoItem.appendChild(todoRemoveBtn);
-    
-//     return todoItem;
-//     };
