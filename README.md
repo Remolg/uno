1e2a895f4022c0eb7e29d1b452474b34557d6877
Remolg <sadikahmet25@gmail.com>
2023-08-08 19:50:50 +0300

    read me edit

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# HomeWork
+
+# Uno
+
+Bu proje HTML5, CSS ve JavaScript kullanarak yapilan basit bir UNO kart oyunudur. Oyun tarayıcınızda oynanabilir. Oyunun amacı, elinizdeki kartları bitirmektir. 
+
+# Nasıl Oynanır
+
+Oyun başladığında her oyuncuda 7 kart vardır.
+
+Ortadaki karta göre sıra sizde ise rengi veya numarası aynı olan kartı ortadaki açık olan kartın üstüne koyabilirsiniz.
+
+Elinizde ortadaki kart ile uyuşan bir kart yoksa, kart çekmek için sağ tarafta bulunan desteden sadece 1 tane kart alabilirsiniz.
+
+Eğer alığınız kart ortada bulunan kart ile eşleşmiyorsa sıra otomatik olarak diğer oyuncuya geçer.
+
+Oyunculardan biri elinde kart kalmadan oyunu bitirirse, o oyuncu kazanır.
+
+
+  
+## Bilgisayarınızda Çalıştırın
+
+Projeyi klonlayın
+
+```bash
+  git clone https://github.com/remolg/Todolist.git
+```
+
+Proje dizinine gidin ve çalıştırın
+
+```bash
+  cd "uno"
+```
+
+## Ekran Görüntüleri
+
+![Uygulama Ekran Görüntüsü](assets/img/uno.png)
--- assets/js/nuno.js
+++ assets/js/nuno.js
@@ -1,11 +1,8 @@
-// Kartlar - biz - karsi oyuncu - cekilecek kartlar ama icini simdilik bos birakiyoruz 
 let cards = [];
 let player = [];
 let cpu = [];
 let deck = [];
 let table;
-
-// For dongusu yapip kartlari olusturuyoruz olusturdugumuz kartlari card dizisine pushluyoruz 
 
 for (let i = 0; i <= 9; i++) {
     cards.push({ color: 'green', number: i.toString() });
@@ -14,79 +11,50 @@ for (let i = 0; i <= 9; i++) {
     cards.push({ color: 'blue', number: i.toString() });
 }
 
-// olusturdugumuz kartlari karistiriyoruz 
-
 cards.sort(function () {
     return Math.random() - 0.5
 });
-
-// karistirdigimiz kartlari for dongusu ile dagitiyoruz ve cards dizisine pushluyoruz shift ile yapmamizin nedeni dizinin 0. elementini almasidir 
 
 for (let i = 1; i <= 7; i++) {
     player.push(cards.shift());
     cpu.push(cards.shift());
 }
 
-// bu kisimda table a cards dizisinin 0. elementi ataniyor 
-
 table = cards.shift();
-
-// bu kisimda ise kalan kartlarin tamami deck elementine ataniyor 
 
 deck = cards;
 
-// sira belirleniyor 
-
 let turn = 1;
 
-// olusturdugumuz kartlari divlere atama ve bunu createCardHtml fonkisyonuna atama <-- data-color ve data-number de olusturulur icerisine verileri atilr -->
 
 function createCardHtml(color, number, back = false) {
     return `<div class="card ${color} ${back ? 'back' : ''}" data-color="${color}" data-number="${number}">${number}</div>`;
 }
-
-// html icersinden secilen calsslari bos nesnelere atama
 
 const cpuContainer = document.querySelector('.cpu');
 const tableContainer = document.querySelector('.table');
 const playerContainer = document.querySelector('.player');
 const deckContainer = document.querySelector('.deck');
 
-// for islemi ile card diye bos bir eleman acilir ve player icerisinde ne kadar eleman var ise o kadar donmesi saglanir her donuste playerin icerisindeki bilgileri tek tek 
-// playercontainer.innerhtml ile oyuncuya verilir
-
-
 for (let card of player) {
     playerContainer.innerHTML += createCardHtml(card.color, card.number);
 }
 
-// cpuContainer.innerhtml ile bilgisayara verilir
-
 for (let card of cpu) {
-    cpuContainer.innerHTML += createCardHtml(card.color, card.number, true);
+    cpuContainer.innerHTML += createCardHtml(card.color, card.number, false);
 }
-
-// deckContainer.innerhtml ile cekilecek kartlara eklenir ve true metodu ile arkasi cevrilir
 
 for (let card of deck) {
-    deckContainer.innerHTML += createCardHtml(card.color, card.number, true);
+    deckContainer.innerHTML += createCardHtml(card.color, card.number, false);
 }
-
-// tableContainer.innerhtml ile tahtaya baslangic karti koyulur
 
 tableContainer.innerHTML += createCardHtml(table.color, table.number);
 
-// butun card classlari cardelemetse atanir 
-
 let cardElements = document.querySelectorAll('.card');
-
-// for dongusu ile hepsini tiklanilabilir yapilir 
 
 for (let cardElement of cardElements) {
     cardElement.addEventListener('click', playCard);
 }
-
-// kartin oynanabilir olup olmamasini kontrol eder tahtadaki kart ile oyuncudaki kartlari kiyaslar
 
 function isCardPlayable(cardColor, cardNumber) {
     if (table.color === cardColor || table.number === cardNumber) {
@@ -95,20 +63,6 @@ function isCardPlayable(cardColor, cardNumber) {
 
     return false;
 }
-
-// changeTurn ile siranin kimde oldugunu belirler ve sirayi degistirir 1 veya 2 degil ise is deck false olur 
-
-function changeTurn() {
-    if (turn === 1) {
-        turn = 2;
-    } else {
-        turn = 1;
-    }
-    isDeckUsed = false;
-}
-
-// canCurrentUserPlay fonksiyonu siradaki kisiyi belirler ve o kisinin bilgilerini currentPlayerCards a atar atanan bilgiler tek tek doner eger kart oynanabilir ise 
-// fonksiyonunda kartlarin rengi ve sayisi kontrol ettirilir oynanabilir ise true doner oynanamaz ise false doner 
 
 function canCurrentUserPlay() {
     let currentPlayerCards;
@@ -127,31 +81,32 @@ function canCurrentUserPlay() {
     return false;
 }
 
+// changeTurn ile siranin kimde oldugunu belirler ve sirayi degistirir 1 veya 2 degil ise is deck false olur 
+
+function changeTurn() {
+    if (turn === 1) {
+        turn = 2;
+    } else {
+        turn = 1;
+    }
+    isDeckUsed = false;
+}
+
+
 let isDeckUsed = false;
 
 // oyun seansi baslar ilk olarak 
 
 function playCard() {
-    // table classina tiklanirsa olacaklar  
     if (this.parentNode.classList.contains('table')) {
         return;
     }
 
-    // deck classina tiklanirsa olacaklar 
-
     if (this.parentNode.classList.contains('deck')) {
-
-        // isdeckused true ise kart cekemessin 
 
         if (isDeckUsed === true) {
             return;
         }
-
-
-        //  mevcut oyuncuda classinda back var ise siler ki kartlarimiz gozuksun 
-
-
-        // mecvut oyuncuya cekilen karti ekletir
 
         if (turn === 1) {
             this.classList.remove('back');
@@ -161,8 +116,6 @@ function playCard() {
         }
 
         isDeckUsed = true;
-
-        // mevcut oyuncu oynayabilecegi bir sey yok ise sirayi degistirir
 
         if (!canCurrentUserPlay()) {
             changeTurn();
@@ -205,7 +158,6 @@ function playCard() {
         gorunurYap[i].classList.remove('back');
     }
 
-
     // table.coloru tikladigin kartin colorune esitle
 
     table.color = this.dataset.color;
@@ -214,23 +166,13 @@ function playCard() {
 
     table.number = this.dataset.number;
 
-    // tikladigin classin at classi 0 ise kazanan oyuncuyu soyle ve 
-
     if (playerCardContainer.children.length === 0) {
         alert('Kazanan Oyuncu: ' + turn);
 
-        // tiklanabilir butun kartlarin tiklama eventini kapatir
-
         cardElements.forEach(card => card.removeEventListener('click', playCard));
-        return; // cok gerekli degil kaldirsak da durum degismez cunku zaten oyun bitti.
+        return;
     }
 
-    // sirayi karsiya ver 
 
     changeTurn();
 }
-
-
-// cpu'nun elindeki karti oynatmak icin
-// [2] yazan yer dinamik olacak
-// document.querySelectorAll('.cpu .card')[2].click();
